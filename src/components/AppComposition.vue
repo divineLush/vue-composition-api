30a594d5cb336b0ce496987bb339fc9832d5ef50
divineLush <alvipath@yahoo.com>
2021-08-31 18:51:00 +0700

    watchers and computed properties

--- src/components/AppComposition.vue
+++ src/components/AppComposition.vue
@@ -1,11 +1,20 @@
 <template>
   <p>{{ num }}</p>
+  <p>{{ double }}</p>
   <button @click.prevent="increment">increment</button>
   <p>{{ name }} {{ age }}</p>
+  <input type="test" v-model="phrase" />
+  <p>{{ reversedPhrase }}</p>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from 'vue';
+import {
+  ref,
+  reactive,
+  toRefs,
+  watch,
+  computed,
+} from 'vue';
 
 export default {
   name: 'AppComposition',
@@ -14,12 +23,14 @@ export default {
     // create reactive reference
     // ref() returns an object
     // ref() is meant for primitive values
-    const num = ref(0);
+    const num = ref(1);
 
     const increment = () => {
       // gotta make changes through value property
       num.value += 1;
     };
+
+    const double = computed(() => num.value * 2);
 
     // reactive() is meant for objects
     const user = reactive({
@@ -32,6 +43,16 @@ export default {
       user.name = 'Doe';
     }, 3000);
 
+    const phrase = ref('');
+    const reversedPhrase = ref('');
+
+    // start watching the phrase variable
+    watch([phrase], ([newVal], [oldVal]) => {
+      console.log(newVal, oldVal);
+      reversedPhrase.value = newVal
+        .split('').reverse().join('');
+    });
+
     // return an object of properties and methods we wish to expose
     return {
       // return entire vairable
@@ -41,6 +62,9 @@ export default {
       // ...user
       // gotta use toRefs() to spread and shorten names in the template
       ...toRefs(user),
+      phrase,
+      reversedPhrase,
+      double,
     };
   },
 };
